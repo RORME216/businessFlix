b16e6a0dcabd88d325b6c8cecc0ca19fe1159c80
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-11 14:16:35 -0300

    feat[heroSection]: created heroSection

---
+++ src/components/section/pageSections/heroSection/heroSection.js
@@ -0,0 +1,15 @@
+import "./heroSectionStyle.css"
+
+export default function HeroSection() {
+    return (
+        <section className="hero-section__container">
+            <div className="hero-section__background"></div>
+            <img className="hero-section__background-image" src="/photos/heroSection/foto_01.jpg"/>
+
+            <div className="hero-section__content">
+                <img className="hero-section_phone-frame" src="/photos/heroSection/cellPhone.png" />
+                <h2 className="hero-section__header-call">Comunicação inovadora que transformar</h2>
+            </div>
+        </section>
+    );
+}
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,3 +1,4 @@
+import "./homepageStyle.css"
 import Footer from "../components/footer/footer";
 import Navbar from "../components/navbar/navbar";
 import NossosParceiros from "../components/section/pageSections/nossosParceiros/nossosParceiros";
@@ -6,6 +7,7 @@ import Contato from "../components/section/pageSections/contato/contato";
 import NossoProduto from "../components/section/pageSections/nossoProduto/nossoProduto";
 import OQueFazemos from "../components/section/pageSections/oqueFazemos/oQueFazemos";
 import Section from "../components/section/section";
+import HeroSection from "../components/section/pageSections/heroSection/heroSection";
 
 export default function Homepage() {
     const cor_azul_escuro = "#002060";
@@ -17,16 +19,17 @@ export default function Homepage() {
 
 
     return(
-        <main>
-            <Navbar />
-
+        <>
+            <Navbar />  
+            <main>
+                <HeroSection />
                 <Section childComponent={<NossoProduto />} backgroundColor={cor_azul_claro}/>
                 <Section childComponent={<OQueFazemos />} backgroundColor={cor_azul_escuro}/>
-                <Section childComponent={<Contato />} backgroundColor={cor_azul_escuro}/>
+                <Section childComponent={<Contato />} backgroundColor={cor_azul_claro}/>
                 <Section childComponent={<Beneficios />} backgroundColor={cor_azul_escuro}/>
                 <Section childComponent={<NossosParceiros />} backgroundColor={cor_azul_claro} />
-
+            </main>
             <Footer />
-        </main>
+        </>
     );
 }
