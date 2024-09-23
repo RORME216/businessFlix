bcdf098d21612c171cb2728017d4beca17fbc47a
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-23 12:19:10 -0300

    feat[footer]: adiciondo resposividade ao footer do site

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,19 +11,18 @@ export default function Footer() {
     return (
         <footer className="footer-container">
             <div className="footer-upper-part">
-                <div className="infos">
+                <div className="contact">
                     <div className="icons-container">
-
                         <a href="mailto:robertoramonmendonca@gmail.com"><MdOutlineEmail className="icon-link" color="white"/></a>
                         <a href="https://www.whatsapp.com"><FaWhatsapp className="icon-link" color="white"/></a>
                        {/* <a href="mailto:email@provedor.com.br?subject=Assunto do email&cc=copia@provedor.com.br&bcc=copiaoculta@provedor.com.br&body=Conteúdo do email que será preenchido automaticamente">Enviar email</a> */}
                     </div>
-                    <div className="organization-infos">
-                        <p className="info-text">São Carlos - Capital da Tecnologia</p>
+                    <div className="Logo">
+                        <BusinessFlixLogo />
                     </div>
                 </div>
-                <div className="Logo">
-                    <BusinessFlixLogo />
+                <div className="organization-infos">
+                    <p className="info-text">São Carlos - Capital da Tecnologia</p>
                 </div>
             </div>
             <div className="footer-lower-part">
