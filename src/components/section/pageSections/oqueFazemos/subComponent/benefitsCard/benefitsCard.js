95c600ff4a76eb3c9f008a7fd5272bf0f38b6dfa
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-25 21:57:25 -0300

    feat[benefitsCards, contato]: corrigido sintaxe de tag img e removido animação de botao em versão monile

--- src/components/section/pageSections/oqueFazemos/subComponent/benefitsCard/benefitsCard.js
+++ src/components/section/pageSections/oqueFazemos/subComponent/benefitsCard/benefitsCard.js
@@ -1,10 +1,13 @@
 import './benefitsCardStyle.css'
 
 export default function BenefitsCard({data}) {
+
+    const alt = `trabalhadores em expediente`
+
     return (
 
         <div className="benefits-card__container">
-            <img className="benefits-card__imagem" src={data.photo} />
+            <img className="benefits-card__imagem" src={data.photo} alt={alt}/>
             <div className="benefits-card__text-container">
                 <p className="benefits-card__text">{data.text}</p>
             </div>
