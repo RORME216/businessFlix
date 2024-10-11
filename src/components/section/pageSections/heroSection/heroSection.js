c356baee34dd1408a46d18dbdeffb9c553c5c063
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-11 11:35:19 -0300

    fix(heroSection): atualizado foto de celular na Hero Section

--- src/components/section/pageSections/heroSection/heroSection.js
+++ src/components/section/pageSections/heroSection/heroSection.js
@@ -7,7 +7,7 @@ export default function HeroSection() {
             <img className="hero-section__background-image" src="/photos/heroSection/foto_01.jpg" alt="Empregados focados trabalhando em equipamentos eletrónicos"/>
 
             <div className="hero-section__content">
-                <img className="hero-section_phone-frame" src="/photos/heroSection/cellPhone.png" alt="Smarth phone exibendo video educativo da empresa"/>
+                <img className="hero-section_phone-frame" src="/photos/heroSection/foto_celular.png" alt="Smarth phone exibendo video educativo da empresa"/>
                 <h2 className="hero-section__header-call">Comunicação inovadora que transforma</h2>
             </div>
         </article>
