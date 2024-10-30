c87ba7624987a38584930a30fcc81b5cb03ef934
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-30 11:04:31 -0300

    feat(contato.js): nova estrutura sessao de contato

--- src/components/section/pageSections/contato/contato.js
+++ src/components/section/pageSections/contato/contato.js
@@ -1,6 +1,8 @@
 import { FaWhatsapp } from "react-icons/fa";
 import "./contatoStyle.css"
-import EmailButton from "./subcomponents/emailModal/emailButton";
+import { MdOutlineEmail } from "react-icons/md";
+
+{/*import EmailButton from "./subcomponents/emailModal/emailButton";*/}
 
 export default function Contato() {
     return(
@@ -13,7 +15,11 @@ export default function Contato() {
                     <a href="https://wa.me/5516988041257" target="_blank" rel="noopener noreferrer">
                         <FaWhatsapp className="contato__whats-app-button" />
                     </a>
-                    <EmailButton />
+                    <a href="mailto:robertoramonmendonca@gmail.com" 
+                    target="_blank" rel="noopener noreferrer">
+                        <MdOutlineEmail className="contato__email-button"/>
+                    </a>
+                    {/*<EmailButton />*/}
                 </div>
 
             </div>
