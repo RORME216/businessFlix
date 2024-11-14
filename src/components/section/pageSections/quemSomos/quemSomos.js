b7ce0dd60e33291cd80420304e0be8aee5496b1b
Eduardo Derisso <dudu.derisso@gmail.com>
2024-11-14 02:43:18 -0300

    feat: added motion to quemSomos section

--- src/components/section/pageSections/quemSomos/quemSomos.js
+++ src/components/section/pageSections/quemSomos/quemSomos.js
@@ -1,8 +1,24 @@
 import "./quemSomosStyle.css"
+import { motion } from 'framer-motion'
+import { useEffect, useState } from "react";
+import { useInView } from 'react-intersection-observer'
 
 export default function QuemSomos() {
+    const [hasHappen, setHasHappen] = useState(false);
+    const {ref, inView} = useInView({threshold: 0.6});
+
     const screemWidth = window.innerWidth;
-    console.log(screemWidth)
+
+    useEffect( () => {
+        if (inView && !hasHappen) {
+            setHasHappen(true);
+        }
+        
+        console.log(" inView:", inView);
+        console.log("hasHappen :", hasHappen );
+        console.log("hasHappen || inView:", hasHappen || inView);
+        console.log("inView && !hasHappen:", inView && !hasHappen);
+    },[inView])
 
     if (screemWidth >= 1025)
         return(
@@ -11,13 +27,17 @@ export default function QuemSomos() {
                     src="/photos/quemSomos/quemSomos.jpg"
                     alt="Imagem de fundo - dois operarios conversando"
                 />
-                <div className="quem-somos__content-auxiliar-container">
-                    <div className="quem-somos__content">
+                <motion.div className="quem-somos__content-auxiliar-container" ref={ref}>
+                    <motion.div className="quem-somos__content"
+                        initial={{x: '1000'}}
+                        animate={{x: (inView || hasHappen) ? 0 : 1000}}
+                        transition={{duration: 2, type: 'spring', bounce: 0.3}}
+                    >
                         <h2 className="quem-somos__title">QUEM SOMOS!</h2>
                         <p className="quem-somos__text">{`Idealizada em 2022 com foco em ESG (Ambiental, Social e Governança), a BusinessFlix se reinventou em janeiro de 2024 para se tornar o que é hoje: uma startup disruptiva em treinamentos audiovisuais, transformando temas essenciais em vídeos curtos, dinâmicos e impactantes. Com nossa metodologia exclusiva, quebramos as barreiras para abordar os mais diversos e desafiadores assuntos, oferecendo soluções que realmente engajam e transformam. Nossa equipe é formada por especialistas altamente qualificados, e temos orgulho de estar em São Carlos, a capital da tecnologia, onde contamos com um ecossistema de inovação que nos conecta aos melhores profissionais do país em todas as etapas: do planejamento à edição, da concepção ao impacto.`}</p>
                         <p className="quem-somos__text">{`Nossa missão é transformar a cultura organizacional das empresas, conscientizando e engajando colaboradores por meio de vídeos que falam a linguagem da era digital. Cada episódio que criamos é uma ferramenta poderosa para inspirar práticas conscientes e desenvolver competências, sempre de forma ágil, relevante e moderna.`}</p>
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
                 
             </article>
     );
