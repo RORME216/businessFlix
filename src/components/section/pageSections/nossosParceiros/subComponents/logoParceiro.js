8ba268fb64e3c66eb685f88d2008e01f17bb5904
Eduardo Derisso <dudu.derisso@gmail.com>
2025-01-09 11:23:44 -0300

    feat: added feature to open parteners companies pages in a new window

--- src/components/section/pageSections/nossosParceiros/subComponents/logoParceiro.js
+++ src/components/section/pageSections/nossosParceiros/subComponents/logoParceiro.js
@@ -2,7 +2,10 @@ import "./logoParceiroStyle.css"
 
 export default function LogoParceiro({link, path, descricao}) {
     return(
-        <a href={link} className="logo-parceiro__anchor">
+        <a href={link} className="logo-parceiro__anchor"
+            rel="noreferrer noopener"
+            target="_blank"
+        >
             <img className="logo-parceiro" src={path} alt={descricao} />
         </a>
     );
