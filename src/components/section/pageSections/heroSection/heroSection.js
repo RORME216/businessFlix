f6371e453a5c1ec1bf5331a609e7e9810f72a7dd
Eduardo Derisso <dudu.derisso@gmail.com>
2025-02-19 09:20:13 -0300

    feat: add logica de mudança de heroSection para mobile

--- src/components/section/pageSections/heroSection/heroSection.js
+++ src/components/section/pageSections/heroSection/heroSection.js
@@ -2,6 +2,37 @@ import "./heroSectionStyle.css"
 import { motion } from 'framer-motion'
 
 export default function HeroSection() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth < 1025)
+        return (
+            <article className="hero-section__container" id="heroSection">
+                
+                <motion.img className="hero-section__background-image"
+                    src="/photos/heroSection/foto_01.jpg"
+                    alt="Empregados focados trabalhando em equipamentos eletrónicos"
+                    initial={{opacity:0}}
+                    animate={{opacity:1}}
+                    transition={{duration: 1}}
+                    />
+    
+                <motion.div className="hero-section__content"
+                    initial={{x:1800}}
+                    animate={{x:0}}
+                    transition={{duration: 2, delay:2}}
+                >
+                    <motion.img className="hero-section_phone-frame"
+                        src="/photos/heroSection/foto_celular.png"
+                        alt="Smarth phone exibendo video educativo da empresa"
+                        initial={{x:-3600}}
+                        animate={{x:0}}
+                        transition={{duration: 2, delay:2}}
+                        />
+                    <h2 className="hero-section__header-call">COMUNICAÇÃO INOVADORA QUE TRANSFORMA</h2>
+                </motion.div>
+            </article>
+        );
+
     return (
         <article className="hero-section__container" id="heroSection">
             
